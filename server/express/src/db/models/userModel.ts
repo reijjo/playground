de7438ba090a654bddf1d05e13af98a369a8ea80
reijjo <teemu.aitomeri@gmail.com>
2025-02-16 15:30:42 +0200

    userschema done

---
+++ server/express/src/db/models/userModel.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { userRoles } from "../../utils/enums";
+
+const userSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		required: [true, "Username is required"],
+		minLength: [3, "Min 3 characters in username"],
+		maxLength: [20, "Max 20 characters in username"],
+	},
+	password: {
+		type: String,
+		required: [true, "Password is required"],
+		minLength: [8, "Min 8 characters in password"],
+		maxLength: [30, "Max 30 characters in password"],
+	},
+	role: {
+		type: String,
+		required: [true, "Role is required"],
+		enum: Object.values(userRoles),
+		default: userRoles.USER,
+	}
+})
+
+export const UserModel = mongoose.model("User", userSchema);
